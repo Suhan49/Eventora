1f40a04181a935566b1238efee941edd3294ff51
Suhan49 <patilsuhan81@gmail.com>
2026-07-28 22:47:00 +0530

    Use Brevo SMTP

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,11 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
 const sendBookingEmail = async (userEmail, userName, eventTitle) => {
